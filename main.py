a5011b74fa22a19637e93e5178ec73bf1864b979
roxiproject <roxiproject@users.noreply.github.com>
2022-10-04 19:58:00 +0000

    split attention regression

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
 // rlhf-distill-experiments — synthetic fixture
-// remove probe perf
-// 8b85a9ac0e08
+// split attention regression
+// d4e148099ad4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
